e1a1b9b77a095ec76a04079771e7d5ef803e4332
alan lima <101990635+alanls1@users.noreply.github.com>
2024-10-23 12:36:05 -0300

    Delete front-end directory

--- front-end/src/app/pesquisa/page.tsx
+++
@@ -1,8 +0,0 @@
-import Search from "@/components/Search/Search";
-import React from "react";
-
-const page = () => {
-  return <Search />;
-};
-
-export default page;
--- front-end/src/test/components/Title/Title.spec.tsx
+++
@@ -1,8 +0,0 @@
-import Home from "@/components/Title/Title";
-import { render, screen } from "@testing-library/react";
-
-test("renders Home component with loading text", () => {
-  render(<Home path="test-path" />);
-
-  expect(screen.getByText("Loading...")).toBeInTheDocument();
-});
